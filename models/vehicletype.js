518a65f44e1aa950d28e5abd15e98a10b691aabe
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-03-02 18:22:45 -0600

    delete unneccessary code

--- models/vehicletype.js
+++ models/vehicletype.js
@@ -1,13 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// async function vehicleTypeCreate(index, type) {
-//     const vehicletype = new VehicleType({ type: type });
-//     await vehicletype.save();
-//     vehicletypes[index] = vehicletype;
-//     console.log(`Added vehicletype: ${type}`);
-//   }
-
 const VehicleTypeSchema = {
   type: {
     type: String,
@@ -16,12 +9,5 @@ const VehicleTypeSchema = {
     message: "Type must be 'Car', 'Truck', or 'SUV'",
   },
 };
-
-// vehicle_type: {
-//     type: String,
-//     required: true,
-//     enum: ["Car", "Truck", "SUV"],
-//     message: "Type must be `Car`, `Truck`, or `SUV`",
-//   },
 
 module.exports = mongoose.model("VehicleType", VehicleTypeSchema)
