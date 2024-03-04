e210d5400d46f1f5314342798d017dad4f38d729
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-03-03 19:55:16 -0600

    created vehicletype controllers

---
+++ controllers/vehicleTypeController.js
@@ -0,0 +1,34 @@
+const VehicleType = require("../models/vehicletype");
+const asyncHandler = require("express-async-handler");
+
+exports.vehicletype_list = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: VehicleType List");
+});
+
+exports.vehicletype_detail = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: VehicleType Details");
+});
+
+exports.vehicletype_create_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: VehicleType create GET");
+});
+
+exports.vehicletype_create_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: VehicleType create POST");
+});
+
+exports.vehicletype_delete_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: VehicleType delete GET");
+});
+
+exports.vehicletype_delete_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: VehicleType delete POST");
+});
+
+exports.vehicletype_update_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: VehicleType update GET");
+});
+
+exports.vehicletype_update_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: VehicleType update POST");
+});
